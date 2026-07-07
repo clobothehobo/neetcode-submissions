02153f92c7f153f9fb3ead9697565c850cdfa4bd
Chloe Lim <cheun0406@gmail.com>
2026-07-07 14:20:24 +1000

    Add: anagram-groups - submission-6

---
+++ Data Structures & Algorithms/anagram-groups/submission-6.py
@@ -0,0 +1,16 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        nstrs = {}
+        for a in strs:
+            na = "".join(sorted(a))
+            if na in nstrs:
+                nstrs[na].append(a)
+            else:
+                nstrs[na] = [a]
+        result = []
+        for n in nstrs:
+            result.append(nstrs.get(n))
+        return result
+
+        
+        
